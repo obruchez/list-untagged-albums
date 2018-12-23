1fa7f80a291f94c7884700d40960e27d68f940be
Olivier Bruchez <olivier@bruchez.org>
2018-12-23 22:41:36 +0100

    Refactor Arguments and add maxAncestorLevelsToCheckForFiles argument

--- src/main/scala/org/bruchez/olivier/listuntaggedalbums/Arguments.scala
+++ src/main/scala/org/bruchez/olivier/listuntaggedalbums/Arguments.scala
@@ -3,71 +3,119 @@ package org.bruchez.olivier.listuntaggedalbums
 import java.nio.file._
 import scala.util._
 
-case class Arguments(path: Path,
+case class Arguments(path: Path = Paths.get("."),
                      extensionsToCheck: Set[String] = Arguments.DefaultExtensionsToCheck,
                      tagsToCheck: Set[String] = Arguments.DefaultTagsToCheck,
-                     filesToCheck: Set[String] = Arguments.DefaultFilesToCheck)
+                     filesToCheck: Set[String] = Arguments.DefaultFilesToCheck,
+                     maxAncestorLevelsToCheckForFiles: Int =
+                       Arguments.DefaultMaxAncestorLevelsToCheckForFiles)
 
 object Arguments {
   protected val DefaultExtensionsToCheck = Set("flac", "m4a", "mp2", "mp3", "mpc", "ogg")
   protected val DefaultTagsToCheck = Set("artist", "title")
   protected val DefaultFilesToCheck = Set[String]()
+  protected val DefaultMaxAncestorLevelsToCheckForFiles = 0
 
-  def apply(args: Array[String]): Try[Arguments] = {
-    if (args.length >= 1) {
-      val path = Paths.get(args(args.length - 1)).toAbsolutePath
+  case class ParsingState(argsToParse: List[String], arguments: Arguments, pathFound: Boolean)
 
-      val defaultArguments = Arguments(path = path)
+  // scalastyle:off cyclomatic.complexity
+  private def nextState(state: ParsingState,
+                        arg: String,
+                        remainingArgs: List[String]): Try[ParsingState] =
+    arg match {
+      case ExtensionsArgument if remainingArgs.nonEmpty =>
+        val extensions = setFromString(remainingArgs.head)
+        Success(
+          state.copy(argsToParse = remainingArgs.tail,
+                     arguments = state.arguments.copy(extensionsToCheck = extensions)))
+      case TagsArgument if remainingArgs.nonEmpty =>
+        val tags = setFromString(remainingArgs.head)
+        Success(
+          state.copy(argsToParse = remainingArgs.tail,
+                     arguments = state.arguments.copy(tagsToCheck = tags)))
+      case FilesArgument if remainingArgs.nonEmpty =>
+        val files = setFromString(remainingArgs.head)
+        Success(
+          state.copy(argsToParse = remainingArgs.tail,
+                     arguments = state.arguments.copy(filesToCheck = files)))
+      case MaxLevelArgument if remainingArgs.nonEmpty =>
+        Try(remainingArgs.head.toInt) match {
+          case Success(maxLevel) =>
+            Success(
+              state.copy(argsToParse = remainingArgs.tail,
+                         arguments =
+                           state.arguments.copy(maxAncestorLevelsToCheckForFiles = maxLevel)))
+          case Failure(_) =>
+            Failure(
+              new Exception(s"Integer expected after $MaxLevelArgument: ${remainingArgs.head}"))
+        }
+      case pathString if remainingArgs.isEmpty =>
+        val path = Paths.get(pathString).toAbsolutePath
+        Success(
+          state.copy(argsToParse = remainingArgs,
+                     arguments = state.arguments.copy(path = path),
+                     pathFound = true))
+      case _ =>
+        Failure(new IllegalArgumentException(s"Unexpected argument: $arg"))
+    }
+  // scalastyle:on cyclomatic.complexity
 
-      fromArgs(args = args.slice(0, args.length - 2).toList, arguments = defaultArguments)
-    } else {
-      Failure(new IllegalArgumentException("Path missing"))
+  @annotation.tailrec
+  def parse(state: ParsingState): Try[ParsingState] = {
+    state.argsToParse match {
+      case Nil =>
+        Success(state)
+      case arg :: remainingArgs =>
+        nextState(state, arg, remainingArgs) match {
+          case Failure(throwable) =>
+            Failure(throwable)
+          case Success(nextState) =>
+            parse(nextState)
+        }
     }
   }
 
-  // scalastyle:off cyclomatic.complexity
-  @annotation.tailrec
-  private def fromArgs(args: List[String], arguments: Arguments): Try[Arguments] =
-    args match {
-      case Nil =>
-        Success(arguments)
-      case arg :: remainingArgs =>
-        (arg match {
-          case ExtensionsArgument if remainingArgs.nonEmpty =>
-            val extensions = setFromString(remainingArgs.head)
-            Success((arguments.copy(extensionsToCheck = extensions), remainingArgs.tail))
-          case TagsArgument if remainingArgs.nonEmpty =>
-            val tags = setFromString(remainingArgs.head)
-            Success((arguments.copy(tagsToCheck = tags), remainingArgs.tail))
-          case FilesArgument if remainingArgs.nonEmpty =>
-            val files = setFromString(remainingArgs.head)
-            Success((arguments.copy(filesToCheck = files), remainingArgs.tail))
-          case _ =>
-            Failure(new IllegalArgumentException(s"Unexpected argument: $arg"))
-        }) match {
-          case Success((newArguments, argsLeftToParse)) =>
-            fromArgs(argsLeftToParse, newArguments)
-          case Failure(throwable) =>
-            Failure(throwable)
-        }
+  def apply(args: Array[String]): Try[Arguments] = {
+    val initialState = ParsingState(args.toList, Arguments(), pathFound = false)
+
+    parse(initialState) match {
+      case Success(state) if !state.pathFound =>
+        Failure(new IllegalArgumentException("Path missing"))
+      case Success(state) =>
+        Success(state.arguments)
+      case Failure(throwable) =>
+        Failure(throwable)
     }
-  // scalastyle:on cyclomatic.complexity
+  }
 
   private def setFromString(string: String): Set[String] =
     string.split(",").map(_.trim.toLowerCase).toSet
 
-  val usage: String =
-    s"""Usage: ListUntaggedAlbums [options] directory
-      |
-      |Options:
-      |
-      |-extensions  comma-separated list of extensions to check (default is ${DefaultExtensionsToCheck.toSeq.sorted
-         .mkString(",")})
-      |-tags tags   comma-separated list of tags to check (default is ${DefaultTagsToCheck.toSeq.sorted
-         .mkString(",")})
-      |-files files comma-separated list of files to check (e.g. cover.jpg) (default is no file)""".stripMargin
-
   private val ExtensionsArgument = "-extensions"
   private val TagsArgument = "-tags"
   private val FilesArgument = "-files"
+  private val MaxLevelArgument = "-max-level"
+
+  private val argumentDescriptions = Seq(
+    ExtensionsArgument ->
+      s"comma-separated list of extensions to check (default is ${DefaultExtensionsToCheck.toSeq.sorted
+        .mkString(",")})",
+    TagsArgument ->
+      s"comma-separated list of tags to check (default is ${DefaultTagsToCheck.toSeq.sorted.mkString(",")})",
+    FilesArgument ->
+      s"comma-separated list of files to check (e.g. cover.jpg) (default is no file)",
+    MaxLevelArgument ->
+      s"maximum number of ancestor directories to check for files (default is $DefaultMaxAncestorLevelsToCheckForFiles)"
+  )
+
+  private val argumentDescriptionsString: String = {
+    val longestArgumentLength = argumentDescriptions.map(_._1.length).max
+
+    argumentDescriptions map {
+      case (argument, description) =>
+        argument + " " * (longestArgumentLength - argument.length + 1) + description
+    } mkString "\n"
+  }
+
+  val usage: String = "Usage: ListUntaggedAlbums [options] directory\n\nOptions:\n\n" + argumentDescriptionsString
 }
